e75e30fccb0d59025c77238fcf38f9e995b6c150
José Torraca <joseneto@eq.ufrj.br>
2024-05-02 16:34:18 +0100

    run

---
+++ run_optuna.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=1:mem=32gb
+#PBS -l walltime=24:00:00
+#PBS -N run_optuna
+
+cd $PBS_O_WORKDIR
+
+module load tools/prod
+# module load SciPy-bundle/2022.05-foss-2022a
+
+python optuna_nmpc_base_v2.py
